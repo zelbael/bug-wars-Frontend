8d66bb8c1d44cc359b7914844795a49301a721a7
Reiczel Bael <reiczelbael.dev@gmail.com>
2024-03-11 07:31:16 -0700

    credits update

--- src/stores/ContributorStore.js
+++ src/stores/ContributorStore.js
@@ -14,14 +14,6 @@ export const useContributorStore = defineStore('contributorStore', {
             picture:'../assets/Reiczel_Bael_headshot.JPG' },
 
             {id: '2',
-            name: 'Berenice Borbon',
-            location: 'Tucson, AZ',
-            linkedin: 'https://www.linkedin.com/in/berenice-borbon/',
-            gitpage: 'https://www.github.com/bborbon',
-            bio: 'Enthusiastic Full-Stack Junior Developer, Passionate about Modern Technologies & AI',
-            picture:'../assets/berenice_borbon_headshot.jpg' },
-
-            {id: '3',
             name: 'Steve Her',
             location: 'Saint Paul, MN',
             linkedin: 'https://www.linkedin.com/in/steveher/',
@@ -29,7 +21,7 @@ export const useContributorStore = defineStore('contributorStore', {
             bio: 'Software Developer, Continuous Learner, Embracing the Power of Problem-Solving and Technological Innovation ',
             picture:'../assets/steve_her_headshot.jpg' },
             
-            {id: '4',
+            {id: '3',
             name: 'Thomas Her',
             location: 'Dallas, TX',
             linkedin: 'https://www.linkedin.com/in/thomaskher/',
@@ -37,23 +29,13 @@ export const useContributorStore = defineStore('contributorStore', {
             bio: 'Aspiring Full Stack Developer with finance background. Hard-working, analytical, problem solver. Enjoys music and fishing',
             picture:'../assets/thomas_her_headshot.jpg' },
 
-            {id: '5',
+            {id: '4',
             name: 'James Ximenez',
             location: 'Austin, TX',
             linkedin: 'https://www.linkedin.com/',
             gitpage: 'https://github.com/',
             bio: 'Full Stack Software Engineer',
             picture:'../assets/james_ximenez_headshot.jpg' },
-
-            {id: '6',
-            name: 'Bianca Flores',
-            location: 'TBD, TBD',
-            linkedin: 'https://www.linkedin.com/',
-            gitpage: 'https://github.com/',
-            bio: 'Full Stack Software Engineer',
-            picture:'../assets/stock_headshot.jpg' },
-
-        
 
         ]
 
